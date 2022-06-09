500e97958333e7abed7c34b2f7c4c229238dca9e
Zacrabo <107219074+Zacrabo@users.noreply.github.com>
2022-06-09 22:38:08 +0100

    scrungled

--- zcrab/mcdonalds/stagehands/randomvehicle.lua
+++
@@ -1,8 +0,0 @@
-function init()
-	local vehicles = config.getParameter("vehicles", {})
-	if math.random() > 0.5 then
-		local v = vehicles[math.random(1, #vehicles)]
-		world.spawnVehicle(v, entity.position(), {ownerKey = sb.makeUuid()})
-	end
-	stagehand.die()
-end
