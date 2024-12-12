1cce4083378c97f146e3de5db4193296260ae8c2
patman <27874300+patmanf@users.noreply.github.com>
2024-12-11 22:54:37 -0600

    fix randomvehicle.lua. whoops

--- zcrab/mcdonalds/stagehands/randomvehicle.lua
+++ zcrab/mcdonalds/stagehands/randomvehicle.lua
@@ -2,7 +2,7 @@ function init()
 	local chance = config.getParameter("vehicleChance", 0.5)
 	if math.random() > chance then
 		local list = config.getParameter("vehicles", {})
-		local name = vehicles[math.random(1, #list)]
+		local name = list[math.random(1, #list)]
 		world.spawnVehicle(name, entity.position(), {ownerKey = sb.makeUuid()})
 	end
 	stagehand.die()
