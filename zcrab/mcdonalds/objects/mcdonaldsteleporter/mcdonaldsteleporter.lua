cd53d95200178d3a9a833ff8518963817e2c1a79
patman <27874300+patmanf@users.noreply.github.com>
2024-12-11 20:26:20 -0600

    mcdonalds teleporter visitors now uses weighted randoms

--- zcrab/mcdonalds/objects/mcdonaldsteleporter/mcdonaldsteleporter.lua
+++ zcrab/mcdonalds/objects/mcdonaldsteleporter/mcdonaldsteleporter.lua
@@ -1,50 +1,90 @@
-require "/scripts/util.lua"
-require "/scripts/vec2.lua"
-
 function init()
-  self.spawnInterval = config.getParameter("spawnInterval")
-  self.spawnPosition = vec2.add(entity.position(), config.getParameter("spawnOffset"))
-	
-	local t = {}
-	for _,npc in ipairs(config.getParameter("npctypes", {})) do
-		if npcCheck(npc.type) then
-			local s = {}
-			
-			for _,species in ipairs(npc.species) do
-				if npcCheck(npc.type, species) then
-					s[#s+1] = species
-				end
+	self.spawnInterval = config.getParameter("spawnInterval")
+	self.spawnPosition = object.toAbsolutePosition(config.getParameter("spawnOffset", {0, 0}))
+
+	resetTimer()
+
+	local filteredNpcs = {}
+	self.totalNpcWeights = 0
+
+	local validSpecies = {}
+	setmetatable(validSpecies, {
+		__index = function(t, key)
+			t[key] = npcExists("base", key)
+			return t[key]
+		end
+	})
+
+	for _, npc in pairs(config.getParameter("npctypes")) do
+		if not npcExists(npc.type) then goto continue end
+
+		local filteredSpecies = {}
+		npc.totalSpeciesWeights = 0
+
+		for _, species in pairs(npc.species) do
+			if type(species) == "string" then
+				species = {name = species, weight = 1}
 			end
-			
-			if #s > 0 then
-				npc.species = s
-				t[#t+1] = npc
+
+			if validSpecies[species.name] then
+				filteredSpecies[#filteredSpecies + 1] = species
+				npc.totalSpeciesWeights = npc.totalSpeciesWeights + (species.weight or 1)
 			end
 		end
+
+		if #filteredSpecies == 0 then goto continue end
+		npc.species = filteredSpecies
+
+		npc.weight = npc.weight or 1
+		self.totalNpcWeights = self.totalNpcWeights + npc.weight
+		filteredNpcs[#filteredNpcs + 1] = npc
+
+		::continue::
 	end
-	
-	self.types = t
+
+	self.npcList = filteredNpcs
 end
 
 function update(dt)
-  if not self.spawnTimer then
-    self.spawnTimer = util.randomInRange(self.spawnInterval)
-  end
-
-  self.spawnTimer = math.max(0, self.spawnTimer - dt)
-  if self.spawnTimer == 0 then
-    local spawn = util.randomFromList(self.types)
-    local species = util.randomFromList(spawn.species)
-    local npcId = world.spawnNpc(self.spawnPosition, species, spawn.type, 1)
-    world.callScriptedEntity(npcId, "status.addEphemeralEffect", "beamin")
-    if spawn.displayNametag then
-      world.callScriptedEntity(npcId, "npc.setDisplayNametag", true)
-    end
-
-    self.spawnTimer = nil
-  end
+	self.spawnTimer = math.max(0, self.spawnTimer - dt)
+	if self.spawnTimer == 0 then
+		spawnNpc()
+		resetTimer()
+	end
 end
 
-function npcCheck(t, s)
-	return pcall(function() root.npcVariant(s or "human", t or "base", 1) end)
+function spawnNpc()
+	local npc = weightedRandom(self.npcList, self.totalNpcWeights)
+	if not npc then return end
+	
+	local species = weightedRandom(npc.species, npc.totalSpeciesWeights)
+	if not species then return end
+
+	local npcId = world.spawnNpc(self.spawnPosition, species.name, npc.type, 1)
+
+	world.callScriptedEntity(npcId, "status.addEphemeralEffect", "beamin")
+	if npc.displayNametag then
+		world.callScriptedEntity(npcId, "npc.setDisplayNametag", true)
+	end
+	
+	return npcId
+end
+
+function resetTimer()
+	self.spawnTimer = self.spawnInterval[1] + (math.random() * (self.spawnInterval[2] - self.spawnInterval[1]))
+end
+
+function npcExists(npcType, species)
+	return pcall(root.npcVariant, species or "human", npcType or "base", 0)
+end
+
+function weightedRandom(options, totalWeight)
+	local choice = math.random() * totalWeight
+	for _, option in ipairs(options) do
+		choice = choice - (option.weight or 1)
+		if choice < 0 then
+			return option
+		end
+	end
+	return nil
 end
